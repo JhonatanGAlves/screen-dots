890a5b10bb59aa7eba515064e8ab12bfcf082975
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-01-31 22:22:25 -0300

    styles: 🎨 Applying styles to the Button and Home components

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,25 @@
 import styled from "styled-components";
 
-export const ButtonContainer = styled.button``;
+export const ButtonContainer = styled.div<{ type: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  padding: 0.5 1rem;
+  margin-bottom: 1.5rem;
+
+  width: 9rem;
+  height: 4rem;
+
+  border: 1px solid black;
+  border-radius: 8px;
+
+  cursor: pointer;
+
+  transition: 0.2s;
+
+  :hover {
+    color: white;
+    background-color: black;
+  }
+`;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,10 +1,20 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.main`
+  display: flex;
+  justify-content: center;
   width: 100%;
   height: 100%;
 
   position: fixed;
   top: 0;
   left: 0;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 12px;
+
+  position: fixed;
+  bottom: 0;
 `;
